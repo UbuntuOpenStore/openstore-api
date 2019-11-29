1fb4247d91916cac0e30b1ef3e22d8016ff66b16
Brian Douglass <bhdouglass@gmail.com>
2019-11-29 16:30:22 -0500

    Improve deploy to staging

--- deploy/post-deploy.sh
+++ deploy/post-deploy.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 VERSION=$1
+ENV=${2:-""}
 
 set -x
 set -e
 
-cd /srv/openstore-api/$VERSION
+cd /srv/openstore-api$ENV/$VERSION
 npm install
 
-echo -e "#!/bin/bash\nexport VERSION=$VERSION" > /srv/openstore/version.sh
+echo -e "#!/bin/bash\nexport VERSION=$VERSION$ENV" > /srv/openstore$ENV/version.sh
 
-rm -f /srv/openstore-api/current
-ln -s /srv/openstore-api/$VERSION /srv/openstore-api/current
+rm -f /srv/openstore-api$ENV/current
+ln -s /srv/openstore-api$ENV/$VERSION /srv/openstore-api$ENV/current
 
-systemctl restart openstore-api
+systemctl restart openstore-api$ENV
 
-cd /srv/openstore-api/
+cd /srv/openstore-api$ENV/
 echo "Going to remove old versions"
 ls -1t | grep -v current | tail -n +10
 ls -1t | grep -v current | tail -n +10 | xargs -d '\n' -r rm -r --
