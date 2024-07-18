9e2fccb3178af373beb50a20775c431678dea26c
Brian Douglass <bhdouglass@gmail.com>
2024-07-18 18:24:34 -0400

    Temporarily disable elastic search

--- src/api/apps.ts
+++ src/api/apps.ts
@@ -24,16 +24,18 @@ async function apps(req: Request, res: Response) {
 
   // Send search queries to elastic search
   // TODO move author searches to a different request parameter
+  /*
   if (filters.search && !filters.search.startsWith('author:') && !filters.search.startsWith('publisher:')) {
     const results = await Package.searchByFilters(filters, getDataBoolean(req, 'full', false));
     pkgs = results.pkgs;
     count = results.count;
   }
   else {
-    const publishedFilters = { ...filters, published: true };
-    pkgs = await Package.findByFilters(publishedFilters, publishedFilters.sort, publishedFilters.limit, publishedFilters.skip);
-    count = await Package.countByFilters(publishedFilters);
-  }
+  */
+  const publishedFilters = { ...filters, published: true };
+  pkgs = await Package.findByFilters(publishedFilters, publishedFilters.sort, publishedFilters.limit, publishedFilters.skip);
+  count = await Package.countByFilters(publishedFilters);
+  // }
 
   const arch = getData(req, 'architecture', Architecture.ARMHF) as Architecture;
   const channel = getData(req, 'channel', DEFAULT_CHANNEL) as Channel;
